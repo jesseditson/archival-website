85256139f2ed9e42b0b028ee6556ccdad504dea1
Jesse Ditson <jesse.ditson@gmail.com>
2024-11-04 14:52:17 -0800

    check in content, fix lightbox

--- public/script.js
+++ public/script.js
@@ -1,9 +1,19 @@
+window.onload = function() {
+    document.querySelectorAll(".photo-item").forEach(function(item) {
+        item.addEventListener("click", function() {
+            const img = item.querySelector("img");
+            openLightbox(img.src, img.getAttribute("alt"))
+        })
+    })
+}
+
 // Open
-function openLightbox(imageUrl) {
+function openLightbox(imageUrl, imageAlt) {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
     
     lightboxImg.src = imageUrl;
+    lightboxImg.alt = imageAlt;
     lightbox.classList.add('active');
 }
 
